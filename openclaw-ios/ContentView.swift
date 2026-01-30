35955ced7d07212b579dc8b051794992cb6d5a91
yunfei07 <yangyunfei07@gmail.com>
2026-01-30 21:42:15 +0800

    Initial Commit

---
+++ openclaw-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  openclaw-ios
+//
+//  Created by yangyunfei on 2026/1/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ openclaw-ios/openclaw_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  openclaw_iosApp.swift
+//  openclaw-ios
+//
+//  Created by yangyunfei on 2026/1/30.
+//
+
+import SwiftUI
+
+@main
+struct openclaw_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
